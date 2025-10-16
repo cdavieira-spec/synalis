b432ffd5ddc3c60d7f5147853064427ae16b47aa
cdavieira-spec <cdavieira@gmail.com>
2025-10-16 11:46:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const imagens = [
   "https://cdn.pixabay.com/photo/2021/05/19/06/13/smartphone-6265046_1280.jpg?auto=format&fit=crop&w=1920&q=80", // palma da mão
   "https://cdn.pixabay.com/photo/2019/05/07/00/07/eye-4184700_1280.jpg?auto=format&fit=crop&w=1920&q=80", // olhos cidade
   "https://cdn.pixabay.com/photo/2019/02/11/02/05/smart-home-3988583_1280.jpg?auto=format&fit=crop&w=1920&q=80", // cidade conectada
-  "https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=1920&q=80", // programadores
+  "https://cdn.pixabay.com/photo/2015/08/05/10/23/traffic-light-876056_1280.jpg?auto=format&fit=crop&w=1920&q=80", // programadores
   "https://images.unsplash.com/photo-1674027444636-ce7379d51252?auto=format&fit=crop&w=1920&q=80", // semáforo inteligente
   "https://images.unsplash.com/photo-1716436329475-4c55d05383bb?auto=format&fit=crop&w=1920&q=80"  // parada de ônibus moderna
 ];
@@ -26,6 +26,7 @@ function trocarImagem() {
 
 trocarImagem(); // define a primeira
 setInterval(trocarImagem, 9000);
+
 
 
 
