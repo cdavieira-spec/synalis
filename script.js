0ffb440d5b98219f522d7604e93dfa23586449c1
cdavieira-spec <cdavieira@gmail.com>
2025-10-16 17:50:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// Alterna imagens automaticamente
+// Alterna imagens no HERO
 let current = 0;
 const images = document.querySelectorAll('.slider img');
+
 function changeImage() {
   images[current].classList.remove('active');
   current = (current + 1) % images.length;
   images[current].classList.add('active');
 }
+
 if (images.length > 0) {
   images[0].classList.add('active');
-  setInterval(changeImage, 4000);
+  setInterval(changeImage, 5000);
 }
