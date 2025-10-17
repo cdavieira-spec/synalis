0c3dadf88b6fd9c2b8027094f87018128008203e
cdavieira-spec <cdavieira@gmail.com>
2025-10-17 09:00:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const images = [
+// Script para mudar imagens do banner dinamicamente
+const imagens = [
   "https://cdn.pixabay.com/photo/2021/05/19/06/13/smartphone-6265046_1280.jpg?auto=format&fit=crop&w=1920&q=80",
   "https://cdn.pixabay.com/photo/2019/05/07/00/07/eye-4184700_1280.jpg?auto=format&fit=crop&w=1920&q=80",
   "https://cdn.pixabay.com/photo/2019/02/11/02/05/smart-home-3988583_1280.jpg?auto=format&fit=crop&w=1920&q=80",
@@ -9,12 +10,8 @@ const images = [
 ];
 
 let index = 0;
-const hero = document.querySelector(".hero");
-
-function changeBackground() {
-  hero.style.backgroundImage = `url(${images[index]})`;
-  index = (index + 1) % images.length;
-}
-
-changeBackground();
-setInterval(changeBackground, 5000);
+setInterval(() => {
+  const hero = document.querySelector(".hero");
+  hero.style.backgroundImage = `url('${imagens[index]}')`;
+  index = (index + 1) % imagens.length;
+}, 6000);
